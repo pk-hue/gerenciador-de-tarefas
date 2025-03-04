c00c1b872d5168209adcbbb9fbf03d6b49936518
Pedro H <ph159.com@gmail.com>
2025-03-04 13:43:53 -0300

    adicionando projeto de gerenciamento de tarefas

---
+++ Gerenciador-de-tarefas.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+struct Tarefa{
+	
+	string descricao;
+	int id;
+	bool completa;
+};
+
+void AdicionarTarefa(){
+	Tarefa tarefa;
+	
+}
+
+int main(){
+	
+	
+	return 0;
+}
